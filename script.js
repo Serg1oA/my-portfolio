c52ae362ec6058288bee01d0518a8fd9f2bc4a6e
Sérgio Almeida <almeida.sergiomc@gmail.com>
2025-09-18 22:57:54 +0200

    Updated content with latest projects and changed style to a dark green

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     project.addEventListener('mouseover', () => {
       buttons.forEach(button => {
-        button.style.backgroundColor = '#333';
-        button.style.color = '#f6f4f1';
-        button.style.border = '2px solid #f6f4f1';
+        button.style.backgroundColor = '#44c8da33';
+        button.style.color = '#EBFDFF';
+        button.style.border = '2px solid #EBFDFF';
         button.style.transition = 'transform 0.5s ease-in-out';
         button.style.animation = 'wiggle 0.3s linear infinite alternate';
       });
@@ -16,9 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     project.addEventListener('mouseout', () => {
       buttons.forEach(button => {
-        button.style.backgroundColor = '#f6f4f1';
-        button.style.color = '#333';
-        button.style.border = '2px solid #333';
+        button.style.backgroundColor = '#031417';
+        button.style.color = '#EBFDFF';
+        button.style.border = '2px solid #EBFDFF';
         button.style.transform = 'rotate(0deg)';
         button.style.animation = 'none';
       });
